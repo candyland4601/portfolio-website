b80b8c4718d17033c1e10d64392951650adf3b8b
Candice <candice.field@hotmail.com>
2019-06-10 16:31:01 -0500

    Edit function

--- :js/scripts.js
+++ :js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hey! Welcome to my portfolio.")
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += 'responsive';
   } else {
     x.className = 'navtoggle';
   }
