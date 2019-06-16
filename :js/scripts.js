49c1d787abbde9f60087e161b054c98d537e656c
Candice <candice.field@hotmail.com>
2019-06-16 13:48:27 -0500

    Correct spacing

--- :js/scripts.js
+++ :js/scripts.js
@@ -1,7 +1,7 @@
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
